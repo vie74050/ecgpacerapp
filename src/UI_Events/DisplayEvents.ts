57af6e0eea121c4a789124cc3fab9b77326b2b14
vie_bcit <vienna_ly@bcit.ca>
2023-03-15 18:51:53 -0700

    Refactor code, add save feature

---
+++ src/UI_Events/DisplayEvents.ts
@@ -0,0 +1,19 @@
+import { GraphMonitor } from "../helpers";
+
+export function Setup(hr_graph: GraphMonitor, bp_graph: GraphMonitor) {
+    const pp_btn = document.getElementById("playpause_btn");
+    pp_btn.onclick = (event) => {
+        hr_graph.PlayPause();
+        bp_graph.PlayPause();
+    }
+
+    const nX = <HTMLInputElement>document.getElementById("nX");
+    nX.onchange = (event) => {
+        let newV = Number((event.target as HTMLInputElement).value);
+        hr_graph.X = -1;
+        hr_graph.nX = newV;
+        bp_graph.X = -1;
+        bp_graph.nX = newV;
+    };
+
+}
